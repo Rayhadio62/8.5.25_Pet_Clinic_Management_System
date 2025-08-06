2d3cf240774911297b6105ed05e2f750446130bd
Ray Kofoed <rayhadio62@gmail.com>
2025-08-06 11:14:21 -0500

    owners

--- pet_vet.py
+++ pet_vet.py
@@ -10,45 +10,47 @@ Session = sessionmaker(bind=engine)
 session = Session()
 
 
+class Pets(Base):
+    __tablename__ = "pets"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    owner_id: Mapped[int] = mapped_column(Integer, ForeignKey("owners.id"))
+    name: Mapped[str] = mapped_column(String(20), nullable=False)
+    species: Mapped[str] = mapped_column(String(10), nullable=False)
+    breed: Mapped[str] = mapped_column(String(20), nullable=False)
+    age: Mapped[int] = mapped_column(Integer, nullable=False)
+    
+    vets: Mapped[list['Vets']] = relationship('Vets', back_populates='pets')
+    owner: Mapped['Owners'] = relationship('Owners', back_populates='pets')
+    appointments: Mapped[list['Appointments']] = relationship('Appointments', back_populates='pet')
+    
+
 class Owners(Base):
     __tablename__ = "owners"
 
     id: Mapped[int] = mapped_column(primary_key=True)
     first_name: Mapped[str] = mapped_column(String(80), nullable=False)
     last_name: Mapped[str] = mapped_column(String(80), nullable=False)
-    phone: Mapped[int] = mapped_column(String(12), nullable=False)
+    phone: Mapped[str] = mapped_column(String(12), nullable=False)
     email: Mapped[str] = mapped_column(String(360), nullable=False)
        
-    vets: Mapped[list['Vets']] = relationship('Vets', back_populates='owner')
-    pets: Mapped[list['Pets']] = relationship('Pets', secondary='vets', back_populates='owners')
-    
-
-class Pets(Base):
-    __tablename__ = "pets"
-
-    id: Mapped[int] = mapped_column(primary_key=True)
-    name: Mapped[str] = mapped_column(String(20), nullable=False)
-    species: Mapped[str] = mapped_column(String(10), nullable=False)
-    breed: Mapped[str] = mapped_column(String(20), nullable=False)
-    age: Mapped[int] = mapped_column(String(3), nullable=False)
-    
-    vets: Mapped[list['Vets']] = relationship('Vets', back_populates='pet')
-    owners: Mapped[list['Owners']] = relationship('Owners', secondary='vets', back_populates='pets')
-    appointments: Mapped[list['Appointments']] = relationship('Appointments', secondary='pet', back_populates='vets')
+    pets: Mapped[list['Pets']] = relationship('Pets', back_populates='owner')
 
 
 class Vets(Base):
     __tablename__ = "vets"
 
     id: Mapped[int] = mapped_column(primary_key=True)
+    pet_id: Mapped[int] = mapped_column(Integer, ForeignKey("pets.id"))
     first_name: Mapped[str] = mapped_column(String(80), nullable=False)
     last_name: Mapped[str] = mapped_column(String(80), nullable=False)
     specialization: Mapped[str] = mapped_column(String(80), nullable=False)
     
-    pets: Mapped[list['Pets']] = relationship('Pets', back_populates='vet')
-    owners: Mapped[list['Owners']] = relationship('Owners', secondary='vets', back_populates='owners')
+    pets: Mapped[list['Pets']] = relationship('Pets', back_populates='vets')
+    appointments: Mapped[list['Appointments']] = relationship('Appointments', back_populates='vet')
     
-class Apointments(Base): #Association Model
+    
+class Appointments(Base):
     __tablename__ = 'appointments'
 
     id: Mapped[int] = mapped_column(primary_key=True)
@@ -58,8 +60,16 @@ class Apointments(Base): #Association Model
     notes: Mapped[str] =mapped_column(String(500))
     status: Mapped[str] =mapped_column(String(20))
     
-    pets: Mapped[list['Pets']] = relationship('Pets', back_populates='appointment')
-    vets: Mapped[list['Vets']] = relationship('Vets', secondary='appointments', back_populates='pet') 
+    pet: Mapped['Pets'] = relationship('Pets', back_populates='appointments')
+    vet: Mapped['Vets'] = relationship('Vets', back_populates='appointments')
+    
+owner_1 = Owners(first_name='Bill', last_name='Gates', phone='111-111-1111', email='bgates@email.com')
+owner_2 = Owners(first_name='Jeff', last_name='Bezos', phone='222-222-2222', email='jbezos@email.com')
+owner_3 = Owners(first_name='Tim', last_name='Cook', phone='333-333-3333', email='tcook@email.com')
+
+
+session.add_all([owner_1, owner_2, owner_3]) 
+session.commit()
 
 
 
@@ -70,4 +80,4 @@ class Apointments(Base): #Association Model
         
     
     
-    Base.metadata.create_all(bind=engine)
+Base.metadata.create_all(bind=engine)
