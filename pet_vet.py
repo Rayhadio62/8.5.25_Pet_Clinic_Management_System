cee5fd633d283988149387249e02dda365807c0f
Ray Kofoed <rayhadio62@gmail.com>
2025-08-06 12:29:41 -0500

    pets

--- pet_vet.py
+++ pet_vet.py
@@ -14,14 +14,15 @@ class Pets(Base):
     __tablename__ = "pets"
 
     id: Mapped[int] = mapped_column(primary_key=True)
-    owner_id: Mapped[int] = mapped_column(Integer, ForeignKey("owners.id"))
+    owner_id: Mapped[int] = mapped_column(Integer, ForeignKey("owners.id"), nullable=True)
     name: Mapped[str] = mapped_column(String(20), nullable=False)
     species: Mapped[str] = mapped_column(String(10), nullable=False)
     breed: Mapped[str] = mapped_column(String(20), nullable=False)
     age: Mapped[int] = mapped_column(Integer, nullable=False)
     
+    
     vets: Mapped[list['Vets']] = relationship('Vets', back_populates='pets')
-    owner: Mapped['Owners'] = relationship('Owners', back_populates='pets')
+    owner: Mapped['Owners'] = relationship('Owners', back_populates='pets')    
     appointments: Mapped[list['Appointments']] = relationship('Appointments', back_populates='pet')
     
 
@@ -63,12 +64,22 @@ class Appointments(Base):
     pet: Mapped['Pets'] = relationship('Pets', back_populates='appointments')
     vet: Mapped['Vets'] = relationship('Vets', back_populates='appointments')
     
-owner_1 = Owners(first_name='Bill', last_name='Gates', phone='111-111-1111', email='bgates@email.com')
-owner_2 = Owners(first_name='Jeff', last_name='Bezos', phone='222-222-2222', email='jbezos@email.com')
-owner_3 = Owners(first_name='Tim', last_name='Cook', phone='333-333-3333', email='tcook@email.com')
+# owner_1 = Owners(first_name='Bill', last_name='Gates', phone='111-111-1111', email='bgates@email.com')
+# owner_2 = Owners(first_name='Jeff', last_name='Bezos', phone='222-222-2222', email='jbezos@email.com')
+# owner_3 = Owners(first_name='Tim', last_name='Cook', phone='333-333-3333', email='tcook@email.com')
 
+pet_1 = Pets(owner_id='1', name='Snoopy', species='dog', breed='beagle', age=74)
+pet_2 = Pets(owner_id='2', name='Sandy', species='dog', breed='Heinz57', age=15)
+pet_3 = Pets(owner_id='3', name='Tom', species='cat', breed='domestic shorthair', age=84)
+pet_4 = Pets(owner_id='1', name='Woody', species='bird', breed='woodpecker', age=85)
+pet_5 = Pets(owner_id='2', name='Garfield', species='cat', breed='orange tabby', age='47')
+pet_6 = Pets(owner_id='3', name='Charlotte', species='araneus cavaticus', breed='barn_spider', age='73') 
 
-session.add_all([owner_1, owner_2, owner_3]) 
+# session.add_all([owner_1, owner_2, owner_3])
+
+session.add_all([pet_1, pet_2, pet_3, pet_4, pet_5, pet_6])
+
+ 
 session.commit()
 
 
